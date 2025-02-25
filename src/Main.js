f3c894cd066404be36566d4ed3e39414738c2b78
Ugam Thakkar <thakkarugam@gmail.com>
2025-02-25 13:25:17 -0800

    Added CSS styles and Basics of state

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,33 @@
 export default function Main(){
+    const ingredients = ["chicken", "paneer", "broth"]
+
+    const ingredientListItems = ingredients.map(ingredient=>(
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("submmitted")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        console.log(newIngredient)
+        ingredients.push(newIngredient)
+    }
     return(
         <main>
-            <form>
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add Ingredient"
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
 
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
     )
 }
